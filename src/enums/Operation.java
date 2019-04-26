04b106b3e96986b08c6c52729c16db7b31a11f1f
Veronica Mascareno <vmascareno@uabc.edu.mx>
2019-04-25 18:51:48 -0700

    Create Operation.java

---
+++ src/enums/Operation.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enums;
+
+/**
+ *
+ * @author vmascareno
+ */
+public enum Operation {
+    ADD, DELETE, UPDATE, GET, GET_ALL, EXIT
+}
