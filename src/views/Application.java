b10e20544307d49af6484823ce229a9fcdc778ca
Veronica Mascareno <vmascareno@uabc.edu.mx>
2019-04-26 01:28:28 -0700

    Update Application.java

--- src/views/Application.java
+++ src/views/Application.java
@@ -14,7 +14,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import models.Graduate;
